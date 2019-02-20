d029732fc0058476760474e6a74dec54db67c5b8
willy <swilly821209@icloud.com>
2019-02-21 02:30:12 +0800

    是要用具名插槽做嗎?

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <TheCard  v-for="(item,key) in list" :key="i"/>
-    </div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheCard v-for="(item,key) in list" :item="item" :key="key">
+      <img :src="item.src" alt>
+    </TheCard>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import TheCard from "@/components/TheCard"
-import '@/assets/reset.css';
+import TheCard from "@/components/TheCard";
+import "@/assets/reset.css";
 
 export default {
   name: "home",
-  data: {
-    list: [
-      {
-        src:"https://cdn-images-1.medium.com/max/500/1*BkRCeibWDrtp2-K3f28q_g.jpeg"
-      }
-    ]
-  
+  data() {
+    return {
+      list: [
+        {
+          src:
+            "https://cdn-images-1.medium.com/max/500/1*BkRCeibWDrtp2-K3f28q_g.jpeg"
+        },
+        {
+          src:
+            "https://cdn-images-1.medium.com/max/500/1*BkRCeibWDrtp2-K3f28q_g.jpeg"
+        },
+        {
+          src:
+            "https://cdn-images-1.medium.com/max/500/1*BkRCeibWDrtp2-K3f28q_g.jpeg"
+        },
+        {
+          src:
+            "https://cdn-images-1.medium.com/max/500/1*BkRCeibWDrtp2-K3f28q_g.jpeg"
+        }
+      ]
+    };
   },
   components: {
     HelloWorld,
-    TheCard,
+    TheCard
   }
 };
 </script>
