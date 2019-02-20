400d107e54d8eaa5928762b169588ce6126ce269
willy <swilly821209@icloud.com>
2019-02-21 00:17:39 +0800

    first

---
+++ src/components/TheCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <slot>header</slot>
+    </div>
+    <div>
+      <slot>body</slot>
+    </div>
+    <div>
+      <slot>foot</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheCard'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,29 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <TheCard  v-for="(item,key) in list" :key="i"/>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import TheCard from "@/components/TheCard"
+import '@/assets/reset.css';
 
 export default {
   name: "home",
+  data: {
+    list: [
+      {
+        src:"https://cdn-images-1.medium.com/max/500/1*BkRCeibWDrtp2-K3f28q_g.jpeg"
+      }
+    ]
+  
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    TheCard,
   }
 };
 </script>
